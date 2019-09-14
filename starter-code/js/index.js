4f17801b753f7a4279f5101febbb2903d2d8c3e8
Alberto Hernández <albertohdezherrera@gmail.com>
2019-09-14 17:07:04 +0200

    hecho en clase

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,17 +1,30 @@
 function deleteItem(e){
-
+  e.target.parentNode.parentNode.remove()
 }
 
+
+// Que me devuelva el precio unitario de cada producto
 function getPriceByProduct(itemNode){
+  const productUnit = itemNode.querySelector(".quantity input").value
+  const productUnitPrice = itemNode.querySelector(".price span").innerText
+  return productUnit * parseInt(productUnitPrice)
+  
+}
+
+// Actualiza el precio total de producto
+function updatePriceByProduct(price, index){
+  const productPrices = document.querySelectorAll('.total-price')[index]
+  productPrices.innerText = `$${price}`
 
 }
 
-function updatePriceByProduct(productPrice, index){
-
-}
-
+//Suma de todos de todo los productos
 function getTotalPrice() {
-
+  const totalProducts = Array.from(document.querySelectorAll(".product"))
+  const totalPrices = totalProducts.reduce((acc, product) => acc + getPriceByProduct(product),0)
+  
+  totalProducts.forEach((e, i) => updatePriceByProduct(getPriceByProduct(e), i))
+  document.querySelector('h2 span').innerText = totalPrices
 }
 
 function createQuantityInput(){
@@ -44,7 +57,7 @@ window.onload = function(){
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  // createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
