8e5187e80ecd14f743cc2a9a76ce7eb38d3e0ce9
unknown <rober.araya.gil@gmail.com>
2019-09-17 19:26:45 +0200

    Day 2

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -28,15 +28,38 @@ function getTotalPrice() {
 }
 
 function createQuantityInput(){
+  const div = document.createElement('div')
+  div.classList.add('quantity')
+  const label = document.createElement('label')
+  label.innerText('QTY')
+  const input = document.createElement('input')
+  input.setAttribute("type", "number")
+  .setAttribute("value","0")
+  .setAttribute("min","0")
+  div.appendChild(label)
+  div.appendChild(input)
+
+  return div
 
 }
 
 function createDeleteButton(){
+  const div = document.createElement('div')
+  div.classList.add('delete')
 
+  const button = document.createElement('button')
+  button.classList.add('btn-delete', 'btn')
+  button.innerText('Delete')
+  div.appendChild(button)
+
+  return div
 }
 
 function createQuantityNode(){
-
+  const div = document.createElement('div')
+  div.classList.add('total-price')
+  div.innerText('0')
+  return div
 }
 
 function createItemNode(dataType, itemData){
@@ -45,10 +68,29 @@ function createItemNode(dataType, itemData){
 
 function createNewItemRow(itemName, itemUnitPrice){
 
+  const divName = document.createElement('div')
+  divName.classList.add('name')
+  divName.innerText(itemName)
+
+  const divPrice = document.createElement('div')
+  divPrice.classList.add('price')
+  divPrice.innerHTML('$<span>' + itemUnitPrice + '</span>')
+
+  const divContainer = document.querySelector('.container')
+  const divProduct = document.createElement('div')
+  divProduct.classList.add('product')
+  divProduct.appendChild(divName)
+  divProduct.appendChild(divPrice)
+  divProduct.appendChild(createQuantityInput())
+  divProduct.appendChild(createQuantityNode())
+  divProduct.appendChild(createDeleteButton())
+  divContainer.appendChild(divProduct)
 }
 
 function createNewItem(){
-
+  const name = document.getElementById('nameInput').value
+  const price = document.getElementById('priceInput').value
+  createNewItemRow(name,price)
 }
 
 window.onload = function(){
@@ -57,7 +99,7 @@ window.onload = function(){
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  // createItemButton.onclick = createNewItem;
+  createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
